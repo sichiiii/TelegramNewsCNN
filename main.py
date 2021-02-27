789f571cae514972a55250e16d05eb3edd9d99f3
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-02-27 19:20:11 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests, telebot, re 
+from bs4 import BeautifulSoup
+from random import randint
+
+bot = telebot.TeleBot('1605853735:AAGYGN3uWIGJO4MY3vCTMX1qnAjNL80U8UY')
+
+@bot.message_handler(commands=['start'])
+def startMessage(message):
+    url = 'http://lite.cnn.com/en'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, features="lxml")
+    rand = randint(1, 100)
+    selector = str(soup.select_one(f'#mount > div > div.afe4286c > ul > li:nth-child({rand})'))
+    articleHref = str(re.findall("href=[\"\'](.*?)[\"\']", selector))
+    articleHref = articleHref.replace("'", '')
+    articleHref = articleHref.replace("[", '')
+    articleHref = articleHref.replace("]", '')
+    articleUrl = "http://lite.cnn.com" + articleHref
+    getTextArticle(message, articleUrl)
+
+def getTextArticle(message, articleUrl):
+    getArticle = requests.get(articleUrl)
+    finalSoup = BeautifulSoup(getArticle.content, features="lxml")
+    final = str(finalSoup.select_one('#mount > div > div.afe4286c').get_text())
+    checkLength(message, final)
+
+def checkLength(message, final):
+    if len(final) > 4096:
+        for x in range(0, len(finalz), 4096):
+            bot.send_message(message.chat.id, final[x:x+4096])
+    else:
+        bot.send_message(message.chat.id, final)
+
+bot.polling(none_stop=True)
